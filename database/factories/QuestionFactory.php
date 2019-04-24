f706c370ab692581ea3e81002c05a47f36dea513
tatsurodev <contacts@tatsuro.dev>
2019-04-24 23:51:51 +0900

    QuestionFactory作成(7)

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+use App\Question;
+
+$factory->define(Question::class, function (Faker $faker) {
+    return [
+        // 末尾のピリオド削除
+        'title' => rtrim($faker->sentence(rand(5, 10)), "."),
+        // paragraphsの第二引数trueで文字列、falseで配列で返す
+        'body' => $faker->paragraphs(rand(3, 7), true),
+        'views' => rand(0, 10),
+        'answers' => rand(0, 10),
+        'votes' => rand(-3, 10)
+    ];
+});
