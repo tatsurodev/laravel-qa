292b77f5d8a3de12177b6ff915e97a5f3005f516
tatsurodev <contacts@tatsuro.dev>
2019-04-26 23:54:08 +0900

    QuestionsController@edit,update,destroyにpolicy適用(23)

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -78,6 +78,7 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
+        $this->authorize('update', $question);
         return view('questions.edit', compact('question'));
     }
 
@@ -90,6 +91,7 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        $this->authorize('update', $question);
         $question->update($request->only('title', 'body'));
         return redirect()->route('questions.index')->with('success', 'Your question has been updated.');
     }
@@ -102,6 +104,7 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
+        $this->authorize('delete', $question);
         $question->delete();
         return redirect()->route('questions.index')->with('success', 'Your question has been deleted.');
     }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -32,7 +32,10 @@
                             <div class="d-flex align-items-center">
                                 <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                 <div class="ml-auto">
+                                    @can('update', $question)
                                     <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                    @endcan
+                                    @can('delete', $question)
                                     <form class="form-delete" action="{{ route('questions.destroy', $question->id) }}" method="post">
                                         @method("DELETE")
                                         @csrf
@@ -40,6 +43,7 @@
                                             DELETE
                                         </button>
                                     </form>
+                                    @endif
                                 </div>
                             </div>
                             <p class="lead">
