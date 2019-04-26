18a4b21002483ae4db55649e692aee57fbc61c53
tatsurodev <contacts@tatsuro.dev>
2019-04-26 16:40:27 +0900

    QuestinsController@editとそのビュー、リンク作成

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -68,7 +68,7 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-        //
+        return view('questions.edit', compact('question'));
     }
 
     /**
