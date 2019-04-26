bdb3111f0f9e67dc8e16bea4cadd3a3573cbb1ec
tatsurodev <contacts@tatsuro.dev>
2019-04-26 15:35:26 +0900

    QuestionsController@storeとメッセージのパーシャルビュー作成(17)

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -44,7 +44,9 @@ class QuestionsController extends Controller
      */
     public function store(AskQuestionRequest $request)
     {
-        dd('store');
+        $request->user()->questions()->create($request->only('title', 'body'));
+        // withでセッション格納
+        return redirect()->route('questions.index')->with('success', 'Your question has been submitted.');
     }
 
     /**
