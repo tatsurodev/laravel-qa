63a3bc8e2fa41978c55caaad95d005c6044230dc
tatsurodev <contacts@tatsuro.dev>
2019-04-26 11:46:45 +0900

    answerカウンターの装飾をアクセサで場合分け(13)

--- app/Question.php
+++ app/Question.php
@@ -30,4 +30,15 @@ class Question extends Model
     {
         return $this->created_at->diffForHumans();
     }
+    // answerカウンターの装飾用にアクセサ定義
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return "answered-accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
+    }
 }
