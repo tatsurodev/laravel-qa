0d508c9fdb323b253a95fc8f9d7a6c5af932ec21
tatsurodev <contacts@tatsuro.dev>
2019-05-04 03:15:30 +0900

    削除するanswerがベストアンサーになっている時、関連するquestionのbest_answer_idカラムを外部キー設定によってnullにし、answerを削除(38)

---
+++ database/migrations/2019_05_03_170031_add_foreign_best_answer_id_to_questions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignBestAnswerIdToQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            // questions.best_answer_idのデータ型をanswers.idのbigIncrementsに揃える
+            $table->unsignedBigInteger('best_answer_id')->change();
+            // 外部キー設定
+            $table->foreign('best_answer_id')
+                ->references('id')
+                ->on('answers')
+                ->onDelete('SET NULL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->unsignedBigInteger('best_answer_id')->change();
+            $table->dropForeign(['best_answer_id']);
+        });
+    }
+}
