f2e3353b2eb3a4e244b4db42260b7972963f24f9
tatsurodev <contacts@tatsuro.dev>
2019-05-04 00:52:44 +0900

    AnswersController@update作成(35)

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -49,9 +49,13 @@ class AnswersController extends Controller
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Answer $answer)
+    public function update(Request $request, Question $question, Answer $answer)
     {
-        //
+        $this->authorize('update', $answer);
+        $answer->update($request->validate([
+            'body' => 'required',
+        ]));
+        return redirect()->route('questions.show', $question->slug)->with('success', 'Your answer has been updated.');
     }
 
     /**
