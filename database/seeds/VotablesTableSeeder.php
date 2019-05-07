07ef31caf00b7208af6da1ab0f068586618f5a4d
tatsurodev <contacts@tatsuro.dev>
2019-05-07 22:17:38 +0900

    vote機能のvotablesテーブルにシーダー作成(48)

---
+++ database/seeds/VotablesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use App\Question;
+
+class VotablesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 元からあるQuestionのvoteレコードを削除
+        \DB::table('votables')->where('votable_type', 'App\Question')->delete();
+
+        $users = User::all();
+        $numberOfUsers = $users->count();
+        $votes = [-1, 1];
+
+        // 全questionがユーザーからランダムにvoteを受ける
+        foreach (Question::all() as $question) {
+            for ($i = 0; $i < rand(0, $numberOfUsers); $i++) {
+                $user = $users[$i];
+                $user->voteQuestion($question, $votes[rand(0, 1)]);
+            }
+        }
+    }
+}
